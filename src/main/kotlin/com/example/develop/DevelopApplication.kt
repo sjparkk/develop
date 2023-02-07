0d54b6ca31d46be9ec8608da00c89d52ad9db064
sjparkk <sjparkk0820@naver.com>
2023-02-07 21:14:40 +0900

    init

---
+++ src/main/kotlin/com/example/develop/DevelopApplication.kt
@@ -0,0 +1,11 @@
+package com.example.develop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevelopApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevelopApplication>(*args)
+}
