d604e1f84cfbcf6fa7db4e5c58ebb8f22f76656e
sjparkk <sjparkk0820@naver.com>
2023-02-09 21:56:54 +0900

    - URISyntaxException & MalformedURLException 발생 test

--- src/test/kotlin/com/example/develop/url/UriTest.kt
+++ src/test/kotlin/com/example/develop/url/UriTest.kt
@@ -1,12 +1,33 @@
 package com.example.develop.url
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.net.MalformedURLException
 import java.net.URI
+import java.net.URISyntaxException
 import java.net.URL
 
 class UriTest {
 
-    private val target = "https://www.google.com/search?q=java+uri+url&oq=java+uri+url"
+    private var target = "https://www.google.com/search?q=java+uri+url&oq=java+uri+url"
+
+    @Test
+    fun `url 형식 안 맞을 시 MalformedURLException 발생`() {
+        Assertions.assertThrows(MalformedURLException::class.java) {
+            target = "exce@$target"
+
+            val url = URL(target)
+        }
+    }
+
+    @Test
+    fun `uri 형식 안 맞을 시 URISyntaxException 발생`() {
+        Assertions.assertThrows(URISyntaxException::class.java) {
+            target = "exce@$target"
+
+            val url = URI(target)
+        }
+    }
 
     @Test
     fun `java url & uri test`() {
