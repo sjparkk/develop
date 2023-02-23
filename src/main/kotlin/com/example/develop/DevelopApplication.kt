c145f34f9a641fea7177cf3ec4f130617729b576
sjparkk <sjparkk0820@naver.com>
2023-02-23 21:34:39 +0900

    - @ConfigurationProperties 스프링부트 버전 2.2 전 후 차이 정리

--- src/main/kotlin/com/example/develop/DevelopApplication.kt
+++ src/main/kotlin/com/example/develop/DevelopApplication.kt
@@ -1,9 +1,26 @@
 package com.example.develop
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+/**
+ * Properties Class 설정
+ *
+ * @EnableConfigurationProperties(BeforeBindingProperties::class)
+ * - @ConfigurationProperties
+ * - @ConstructorBinding 사용 가능
+ * - Properties Class 많아지게되면 코드 무거워지는 단점
+ *
+ * @ConfigurationPropertiesScan
+ * - 스프링부트 2.2 부터 지원
+ * - @EnableConfigurationProperties or @Component 없이 사용 가능
+ * - @ConstructorBinding 사용 가능
+ * - @ConstructorBinding 사용하여 Immutable 한 Properties 객체 보장 가능
+ */
+//@EnableConfigurationProperties(BeforeBindingProperties::class)
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class DevelopApplication
 
 fun main(args: Array<String>) {
