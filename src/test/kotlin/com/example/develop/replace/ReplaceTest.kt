1231c6e458c53277905d6b4941d83d73626450b1
sjparkk <sjparkk0820@naver.com>
2024-01-12 22:56:52 +0900

    feat : 문자열 치환 기본 테스트

--- src/test/kotlin/com/example/develop/replace/ReplaceTest.kt
+++ src/test/kotlin/com/example/develop/replace/ReplaceTest.kt
@@ -1,8 +1,18 @@
 package com.example.develop.replace
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 /**
  * 코틀린 표준 라이브러리에 포함된 String.replace에 대한 테스트 및 정리
  */
 class ReplaceTest {
+
+    @Test
+    fun `문자열 replace 기본 테스트`() {
+        val originalString = "Hello, World!"
+        val replacedString = originalString.replace("Hello", "Hi")
+        Assertions.assertEquals("Hi, World!", replacedString)
+    }
 
 }
