58fd5d7b006a85de1a324b8a429e56a16656f42f
sjparkk <sjparkk0820@naver.com>
2024-01-12 23:04:43 +0900

    test : replace에서 다중으로 처리 되는 경우 내부 동작 확인

--- src/test/kotlin/com/example/develop/replace/ReplaceTest.kt
+++ src/test/kotlin/com/example/develop/replace/ReplaceTest.kt
@@ -15,7 +15,35 @@ class ReplaceTest {
         Assertions.assertEquals("Hi, World!", replacedString)
     }
 
+    /**
+     * replace 함수에서 일치하는 것 모두 바뀌는 이유?
+     *
+     * replace 함수 내부 구현 코드
+    @Suppress("ACTUAL_FUNCTION_WITH_DEFAULT_ARGUMENTS")
+    public actual fun String.replace(oldValue: String, newValue: String, ignoreCase: Boolean = false): String {
+        run {
+            var occurrenceIndex: Int = indexOf(oldValue, 0, ignoreCase)
+            // FAST PATH: no match
+            if (occurrenceIndex < 0) return this
 
+            val oldValueLength = oldValue.length
+            val searchStep = oldValueLength.coerceAtLeast(1)
+            val newLengthHint = length - oldValueLength + newValue.length
+            if (newLengthHint < 0) throw OutOfMemoryError()
+            val stringBuilder = StringBuilder(newLengthHint)
+
+            var i = 0
+            do {
+                stringBuilder.append(this, i, occurrenceIndex).append(newValue)
+                i = occurrenceIndex + oldValueLength
+                if (occurrenceIndex >= length) break
+                occurrenceIndex = indexOf(oldValue, occurrenceIndex + searchStep, ignoreCase)
+            } while (occurrenceIndex > 0)
+            return stringBuilder.append(this, i, length).toString()
+        }
+    }
+     *  여기서 중요한 부분은 do while문 을 통해서 일치하는 oldValue가 더 이상 발생하지 않을 때까지 루프가 계속 되어 치환 되는 것
+     */
     @Test
     fun `코틀린에서 replace는 일치하는 것 모두 바꿈`() {
         val originalString = "Hello, Hello, Hello, World!"
